107816e281e629932f6d89fb6c6eed52ecaec6e6
abdulloh <abdullohprogrammer@gmail.com>
2023-08-23 02:18:43 +0500

    asdfg

---
+++ js/main.js
@@ -0,0 +1,116 @@
+//    1-misol   //
+
+// let k = 10;
+// let n = 4;
+
+// for(let i = 1; i <= n; i++) {
+//    console.log(k);
+// }
+
+
+
+
+
+//    2-misol    //
+
+// let a = 5;
+
+// let b = 20;
+
+
+// for(let i = a; i <= b;i++) {
+//    console.log(i);
+// }
+
+
+
+
+//    3-misol    //
+
+// let a = 20;
+
+// let b = 5;
+
+
+// for(let i = a; i <= b;i++) {
+//    console.log(i);
+// }
+
+
+
+
+//     4-misol     //
+
+   // let narxlar = ['1kg=10000 som','2kg=20000 som','3kg=30000 som','4kg=40000 som','5kg=50000 som','6kg=60000 som','7kg=70000 som','8kg=80000 som','9kg=90000 som','10kg=100000 som']
+
+   // for(leti = 0; i < 10; i++) {
+   //    console.log(narxlar[i]);
+   // }
+
+
+
+   //     5-misol    //
+
+   // let narxlar = ['0,1kg=1000 som','0,2kg=2000 som','0,3kg=3000 som','0,4kg=4000 som','0,5kg=5000 som','0,6kg=6000 som','0,7kg=7000 som','0,8kg=8000 som','0,9kg=9000 som','1kg=100000 som']
+
+   // for(leti = 0; i < 10; i++) {
+   //    console.log(narxlar[i]);
+   // }
+
+
+
+//    6-misol    //
+
+// let a = 0.1;
+// let b = 2;
+
+// for (let i = a; i <= b; i++) {
+//    if (i % 0.2 ==0) {
+//       console.log(i);
+//    }
+// }
+
+
+
+//     7-misol     //
+
+
+// let a = 5;
+
+// let b = 20;
+
+
+// for(let i = a; i <= b;i++) {
+//    console.log(i && a + b);
+// }
+
+
+
+
+//     8-misol   //
+
+
+
+// let a = 5;
+
+// let b = 20;
+
+
+// for(let i = a; i <= b;i++) {
+//    console.log(i && a * b);
+// }
+
+
+//    9-misol    //
+
+
+// let n = 40;
+
+
+// for (let i = n;  i++) {
+//    if (i % 3 ==0) {
+//       console.log(i);
+//    }
+// }
+
+
